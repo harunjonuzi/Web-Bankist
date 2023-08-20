0cb82bd75f3f9bac4fbb5820448d530b8e540502
Harun-Mac <iamharunjonuzi@gmail.com>
2023-08-20 11:09:56 +0200

    feat: added mouseover reduce opacity on nav links

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 // * 2.0 Modal window
 const openModal = function (e) {
@@ -105,9 +109,6 @@ btnScrollTo.addEventListener("click", function (e) {
 });
 
 // * 5.0 Tabbed Component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 // TODO With event Delegation
 tabsContainer.addEventListener("click", function (e) {
@@ -141,3 +142,44 @@ tabsContainer.addEventListener("click", function (e) {
 //     console.log("Click");
 //   })
 // );
+
+// * 6.0 Mouseover nav links
+
+// const handleHover = function (e) {
+//   if (e.target.classList.contains("nav__link")) {
+//     const link = e.target;
+//     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+//     const logo = link.closest(".nav").querySelector("img");
+
+//     siblings.forEach((el) => {
+//       if (el !== link) el.style.opacity = this;
+//     });
+
+//     logo.style.opacity = this;
+//   }
+// };
+
+// nav.addEventListener("mouseover", handleHover.bind(0.5));
+// nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Less Complicated
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const siblings = e.target.closest(".nav").querySelectorAll(".nav__link");
+    const logo = e.target.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== e.target) el.style.opacity = opacity;
+    });
+
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener("mouseover", function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", function (e) {
+  handleHover(e, 1);
+});
